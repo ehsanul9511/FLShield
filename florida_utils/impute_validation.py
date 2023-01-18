db174a8c04941a413b08be40df94f857ade8f259
Ehsanul Kabir <ehsanul.kabir.stn@gmail.com>
2023-01-18 05:19:00 -0500

    imputation fixed

--- florida_utils/impute_validation.py
+++ florida_utils/impute_validation.py
@@ -17,16 +17,17 @@ def convert_to_numpy(evaluations_of_clusters, count_of_class_for_validator, num_
 
 def calc_mse(actual, pred, missing_mask, method_name):
     mse = ((pred[missing_mask] - actual[missing_mask]) ** 2).mean()
-    logger.info(f"{method_name} MSE: %f" % mse) 
+    # logger.info(f"{method_name} MSE: %f" % mse)
+    return mse
 
 def impute(eval_tensor, impute_method='iterative'):
     if impute_method == 'iterative':
-        imputer = IterativeImputer(n_nearest_features = 5, initial_strategy = 'median', random_state = 42)
+        imputer = IterativeImputer(n_nearest_features = 5, initial_strategy = 'median', random_state = 42, max_iter = 10, tol = 1e-3)
         eval_tensor = imputer.fit_transform(eval_tensor)
     elif impute_method == 'KNN':
-        eval_tensor = KNN(k=3).fit_transform(eval_tensor)
+        eval_tensor = KNN(k=3, verbose=False).fit_transform(eval_tensor)
     elif impute_method == 'SoftImpute':
-        eval_tensor = SoftImpute().fit_transform(eval_tensor)
+        eval_tensor = SoftImpute(verbose=False).fit_transform(eval_tensor)
     elif impute_method == 'SimilarityWeightedAveraging':
         eval_tensor = SimilarityWeightedAveraging().fit_transform(eval_tensor)
     elif impute_method == 'mean':
@@ -39,6 +40,7 @@ def impute(eval_tensor, impute_method='iterative'):
         eval_tensor = SimpleFill('random').fit_transform(eval_tensor)
     else:
         raise NotImplementedError
+    return eval_tensor
 
 
 def impute_validation(evaluations_of_clusters, names, num_of_clusters, num_of_classes, impute_method='iterative'):
@@ -112,6 +114,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--location', type=str, default=None)
     parser.add_argument('--epoch', type=int, default=301)
+    parser.add_argument('--missing_percentage', type=float, default=0.1)
 
     # parse args
     args = parser.parse_args()
@@ -134,16 +137,32 @@ if __name__ == '__main__':
     eval_tensor = convert_to_numpy(evaluations_of_clusters, count_of_class_for_validator, num_of_clusters, num_of_classes, names)
 
     eval_tensor = eval_tensor.reshape((len(names), num_of_clusters*num_of_classes))
+
+    # method_names = ['KNN', 'SoftImpute', 'iterative', 'SimilarityWeightedAveraging', 'mean', 'median', 'zero', 'random']
+    method_names = ['KNN', 'SoftImpute', 'iterative', 'mean', 'median', 'random']
+
+    possible_values = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+    # create a pandas dataframe with the possible values as columns and the method names as rows
+    import pandas as pd
+    from matplotlib import pyplot as plt
+    df = pd.DataFrame(columns=method_names, index=possible_values)
     
-    eval_tensor_incomplete = eval_tensor.copy()
-    mask = np.random.rand(*eval_tensor_incomplete.shape) < 0.1
-    eval_tensor_incomplete[mask] = np.nan
+    for val in possible_values:
+        logger.info(f'missing_percentage: {val}')
+        eval_tensor_incomplete = eval_tensor.copy()
+        mask = np.random.rand(*eval_tensor_incomplete.shape) <  val # args.missing_percentage
+        eval_tensor_incomplete[mask] = np.nan
 
-    method_names = ['KNN', 'SoftImpute', 'iterative', 'SimilarityWeightedAveraging', 'mean', 'median', 'zero', 'random']
+        for method_name in method_names:
+            eval_tensor_imputed = impute(eval_tensor_incomplete, method_name)
+            # logger.info(f'eval_tensor_imputed {method_name}: {eval_tensor_imputed[0]}')
+            mse = calc_mse(eval_tensor, eval_tensor_imputed, mask, method_name)
+            df.loc[val, method_name] = mse
 
-    for method_name in method_names:
-        eval_tensor_imputed = impute(eval_tensor_incomplete, method_name)
-        calc_mse(eval_tensor, eval_tensor_imputed, mask, method_name)
+    logger.info(f'df: {df}')
+    df.plot(marker='o')
+    plt.savefig('imputation.pdf')
 
     # logger.info(f'evaluations_of_clusters: {evaluations_of_clusters[0]}')
 
